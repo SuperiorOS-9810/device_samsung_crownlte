dabed47a43f3b8f845fe3f0fedd8c74468645af1
thenist <p717394@gmail.com>
2022-11-02 22:21:50 +0900

    Adapt to SuperiorOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crownlte.mk
+    $(LOCAL_DIR)/superior_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_crownlte-eng \
-    aosp_crownlte-user \
-    aosp_crownlte-userdebug
+    superior_crownlte-eng \
+    superior_crownlte-user \
+    superior_crownlte-userdebug
